f7fe3677e43c6e870b75454f1697d0fcca5ea2b3
gvantsa999 <gvantsadighmelishvili@gmail.com>
2026-03-28 13:51:20 +0000

    exc

--- loops/comprehensions/comprehensions.py
+++ loops/comprehensions/comprehensions.py
@@ -1,18 +1,22 @@
 from helpers import get_words, save_counts
 
-
 def main():
     counts = {}
     words = get_words("address.txt")
-    lst = [word.lower() for word in words]
+    words = [word.lower() for word in words if len(word) > 4]
 
-    for word in lst:
+    # words = []
+    # words_lst = []
+    # for word in words:
+    #     lower_word = word.lower()
+    #     words_lst.append(lower_word) 
+
+    for word in words: 
         if word in counts:
             counts[word] += 1
         else:
             counts[word] = 1
 
     save_counts(counts)
-
 
 main()
